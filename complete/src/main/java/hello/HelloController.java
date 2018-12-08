5095bce7d7a13082ce6630ccf92cb9cfddac8659
pjs <peter@visibility.net>
2018-12-08 12:36:19 +0000

    Changed text in HelloController

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
-    
+
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from Spring Boot! Let\'s see if we can get Swagger 2 working. Then we can look at generating typescript interfaces";
     }
-    
+
 }
